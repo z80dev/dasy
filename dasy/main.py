1f71a1cedb893372acd19735fc6ceb91cfd329fd
z80 <z80dev@mlnl.finance>
2022-08-10 21:02:38 -0400

    implement filename reading

--- dasy/main.py
+++ dasy/main.py
@@ -1,12 +1,21 @@
 from dasy import compiler
+import argparse
 import sys
 
 def main():
+    parser = argparse.ArgumentParser(prog="dasy")
+    parser.add_argument('filename', type=str, nargs='?', default="")
 
     src = ""
 
-    for line in sys.stdin:
-        src += line
+    args = parser.parse_args()
+
+    if args.filename != "":
+        with open(args.filename, 'r') as f:
+            src = f.read()
+    else:
+        for line in sys.stdin:
+            src += line
 
     data = compiler.compile(src)
     print("0x" + data.bytecode.hex())
