0dbc0473925016c5e83cc5a2047b548006034daf
z80 <z80dev@mlnl.finance>
2022-08-11 14:38:19 -0400

    clean up code with match statements

--- dasy/parser/parse.py
+++ dasy/parser/parse.py
@@ -38,42 +38,39 @@ def parse_fn(fn_tree):
     fn_node_id = next_nodeid()
     assert isinstance(fn_tree, models.Expression)
     assert fn_tree[0] == models.Symbol('defn')
-    name = str(fn_tree[1])
-    args_list = parse_args_list(fn_tree[2])
-    args = vy_nodes.arguments(args=args_list, defaults=list(), node_id=next_nodeid(), ast_type='arguments')
-    rets = vy_nodes.Name(id=fn_tree[3].name, node_id=next_nodeid(), ast_type='Name')
-
-    decorators = []
-    body = []
-    for el in fn_tree[4:-1]: # all but last
-        if isinstance(el, models.Keyword):
-            decorators.append(vy_nodes.Name(id=el.name, node_id=next_nodeid(), ast_type='Name'))
-        elif isinstance(el, models.Expression):
-            body.append(parse_node(el))
-    assert isinstance(fn_tree[-1], models.Expression)
-    value_node = parse_node(fn_tree[-1])
-    implicit_return_node = vy_nodes.Return(value=value_node, ast_type='Return', node_id=next_nodeid())
-
-    body.append(implicit_return_node)
-    return vy_nodes.FunctionDef(args=args, returns=rets, decorator_list=decorators, pos=None, body=body, name=name, node_id=fn_node_id, ast_type='FunctionDef')
+    match fn_tree[1:]:
+        case models.Symbol(sym_node), models.List(list_node), models.Keyword(ret_node), models.Keyword(vis), *body:
+            name = str(sym_node)
+            args_list = parse_args_list(list_node)
+            args = vy_nodes.arguments(args=args_list, defaults=list(), node_id=next_nodeid(), ast_type='arguments')
+            rets = vy_nodes.Name(id=ret_node, node_id=next_nodeid(), ast_type='Name')
+            decorators = [vy_nodes.Name(id=vis, node_id=next_nodeid(), ast_type='Name')]
+            fn_body = [parse_node(body_node) for body_node in body[:-1]]
+            assert isinstance(body[-1], models.Expression)
+            value_node = parse_node(body[-1])
+            implicit_return_node = vy_nodes.Return(value=value_node, ast_type='Return', node_id=next_nodeid())
+            fn_body.append(implicit_return_node)
+        case _:
+            raise Exception(f"Invalid fn form {fn_tree}")
+    return vy_nodes.FunctionDef(args=args, returns=rets, decorator_list=decorators, pos=None, body=fn_body, name=name, node_id=fn_node_id, ast_type='FunctionDef')
 
 def parse_contract(expr):
+    mod_node = vy_nodes.Module(body=[], name=str(expr[1]), doc_string="", ast_type='Module', node_id=next_nodeid())
+    expr_body = []
     match expr[1:]:
         case (name, vars, *body) if isinstance(vars, models.List):
-            # print(f"vars match: {expr}")
-            mod_node = vy_nodes.Module(body=[], name=str(expr[1]), doc_string="", ast_type='Module', node_id=next_nodeid())
-
-            for node in expr[3:]:
-                mod_node.add_to_body(parse_node(node))
-            return mod_node
+            # contract has state
+            expr_body = expr[3:]
         case (name, *body):
-            mod_node = vy_nodes.Module(body=[], name=str(expr[1]), doc_string="", ast_type='Module', node_id=next_nodeid())
-            for node in expr[2:]:
-                mod_node.add_to_body(parse_node(node))
-            return mod_node
+            # no contract state
+            expr_body = expr[2:]
         case _:
             # print(f"no match: {expr}")
             raise Exception(f"Invalid defcontract form: {expr}")
+    for node in expr_body:
+        mod_node.add_to_body(parse_node(node))
+
+    return mod_node
 
 def parse_expr(expr):
     match str(expr[0]):
@@ -105,11 +102,13 @@ def parse_node(node):
         case models.Integer(node):
             value_node = vy_nodes.Int(value=int(node), node_id=next_nodeid(), ast_type='Int')
             return value_node
-        case str(node) if node in BUILTIN_FUNCS:
+        case models.Symbol(node) if str(node) in BUILTIN_FUNCS:
             return parse_builtin(node)
         case models.Symbol(node):
             name_node = vy_nodes.Name(id=str(node), node_id=next_nodeid(), ast_type='Name')
             return name_node
+        case _:
+            raise Exception(f"No match for node {node}")
 
 def parse_src(src: str):
     ast = parse_node(hy.read(src))
