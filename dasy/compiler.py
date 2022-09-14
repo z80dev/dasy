b8a4603d6887d540e631a3169a7419016259a4bc
z80 <z80dev@mlnl.finance>
2022-09-14 16:16:25 -0400

    add abi to compiler output (for ape-dasy)

--- dasy/compiler.py
+++ dasy/compiler.py
@@ -1,8 +1,10 @@
 from vyper.compiler.phases import CompilerData
+from vyper.compiler.output import build_abi_output
 from dasy.parser import parse_src
 
 def compile(src: str) -> CompilerData:
     ast = parse_src(src)
     data = CompilerData("", ast.name, None, source_id=0, )
     data.vyper_module = ast
+    data.__dict__["abi"] = build_abi_output(data)
     return data
