3568f1971047cdd35178f81189908891a181b5dd
z80 <z80dev@mlnl.finance>
2022-09-23 21:55:39 -0400

    add output formats

--- dasy/compiler.py
+++ dasy/compiler.py
@@ -1,9 +1,67 @@
-from vyper.compiler.phases import CompilerData
-from vyper.compiler.output import build_abi_output
+from vyper.compiler.phases import CompilerData as VyperCompilerData
+from vyper.compiler.output import build_abi_output, build_asm_output, build_bytecode_runtime_output, build_external_interface_output, build_interface_output, build_ir_output, build_ir_runtime_output, build_layout_output, build_opcodes_output
 from dasy.parser import parse_src
 
+class CompilerData(VyperCompilerData):
 
-def compile(src: str, include_abi=True) -> CompilerData:
+    def __init__(self, *args, **kwargs):
+        VyperCompilerData.__init__(self, *args, **kwargs)
+
+    @property
+    def runtime_bytecode(self):
+        runtime_bytecode = build_bytecode_runtime_output(self)
+        self.__dict__["runtime_bytecode"] = runtime_bytecode
+        return runtime_bytecode
+
+    @property
+    def abi(self):
+        abi = build_abi_output(self)
+        self.__dict__["abi"] = abi
+        return abi
+
+    @property
+    def interface(self):
+        interface = build_interface_output(self)
+        self.__dict__["interface"] = interface
+        return interface
+
+    @property
+    def ir(self):
+        ir = build_ir_output(self)
+        self.__dict__["ir"] = ir
+        return ir
+
+    @property
+    def runtime_ir(self):
+        ir = build_ir_runtime_output(self)
+        self.__dict__["runtime_ir"] = ir
+        return ir
+
+    @property
+    def asm(self):
+        asm = build_asm_output(self)
+        self.__dict__["asm"] = asm
+        return asm
+
+    @property
+    def opcodes(self):
+        return build_opcodes_output(self)
+
+    @property
+    def runtime_opcodes(self):
+        return build_opcodes_output(self)
+
+    @property
+    def external_interface(self):
+        return build_external_interface_output(self)
+
+    @property
+    def layout(self):
+        return build_layout_output(self)
+
+
+
+def generate_compiler_data(src: str) -> CompilerData:
     ast = parse_src(src)
     data = CompilerData(
         "",
@@ -12,6 +70,12 @@ def compile(src: str, include_abi=True) -> CompilerData:
         source_id=0,
     )
     data.vyper_module = ast
-    if include_abi:
-        data.__dict__["abi"] = build_abi_output(data)
     return data
+
+
+def compile(src: str, include_abi=True) -> CompilerData:
+    data = generate_compiler_data(src)
+    return data
+
+def generate_abi(src: str) -> list:
+    return compile(src).abi
--- dasy/main.py
+++ dasy/main.py
@@ -2,10 +2,41 @@ from dasy import compiler
 import argparse
 import sys
 
+format_help = """Format to print, one or more of:
+bytecode (default) - Deployable bytecode
+bytecode_runtime   - Bytecode at runtime
+abi                - ABI in JSON format
+layout             - Storage layout of a Dasy contract
+ast                - AST in JSON format
+interface          - Vyper interface of a contract
+external_interface - External interface of a contract, used for outside contract calls
+opcodes            - List of opcodes as a string
+opcodes_runtime    - List of runtime opcodes as a string
+ir                 - Intermediate representation in list format
+"""
+
+
+OUTPUT_HANDLERS = {
+    "bytecode": lambda data: print("0x" + data.bytecode.hex()),
+    "runtime_bytecode": lambda data: print(data.runtime_bytecode),
+    "abi": lambda data: print(data.abi),
+    "interface": lambda data: print(data.interface),
+    "ir": lambda data: print(data.ir),
+    "runtime_ir": lambda data: print(data.runtime_ir),
+    "asm": lambda data: print(data.asm),
+    "opcodes": lambda data: print(data.opcodes),
+    "runtime_opcodes": lambda data: print(data.runtime_opcodes),
+    "external_interface": lambda data: print(data.external_interface),
+    "layout": lambda data: print(data.layout)
+}
 
 def main():
-    parser = argparse.ArgumentParser(prog="dasy")
+    parser = argparse.ArgumentParser(prog="dasy",
+                                        description="Lispy Smart Contract Language for the EVM",
+    formatter_class=argparse.RawTextHelpFormatter,
+ )
     parser.add_argument("filename", type=str, nargs="?", default="")
+    parser.add_argument("-f", help=format_help, default="bytecode", dest="format")
 
     src = ""
 
@@ -19,7 +50,10 @@ def main():
             src += line
 
     data = compiler.compile(src)
-    print("0x" + data.bytecode.hex())
+    if args.format in OUTPUT_HANDLERS:
+        OUTPUT_HANDLERS[args.format](data)
+    else:
+        raise Exception(f"Unrecognized Output Format {args.format}. Must be one of {OUTPUT_HANDLERS.keys()}")
 
 
 if __name__ == "__main__":
