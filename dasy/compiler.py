8f62b15509a0022c8cd9c42adc71e8cb2d83f3fd
z80 <z80@ophy.xyz>
2024-02-08 23:50:25 -0500

    fix: make version pragma not mandatory

--- dasy/compiler.py
+++ dasy/compiler.py
@@ -77,19 +77,19 @@ class CompilerData(VyperCompilerData):
 def generate_compiler_data(src: str, name="DasyContract") -> CompilerData:
     (ast, settings) = parse_src(src)
     settings = Settings(**settings)
-    if settings.evm_version:
-        with anchor_evm_version(settings.evm_version):
-            data = CompilerData(
-                "",
-                ast.name or name,
-                None,
-                source_id=0,
-                settings=settings,
-            )
-            # data.settings = settings
-            data.vyper_module = ast
-            _ = data.bytecode
-            return data
+    version = settings.evm_version or "paris"
+    with anchor_evm_version(settings.evm_version):
+        data = CompilerData(
+            "",
+            ast.name or name,
+            None,
+            source_id=0,
+            settings=settings,
+        )
+        # data.settings = settings
+        data.vyper_module = ast
+        _ = data.bytecode
+        return data
 
 
 def compile(src: str, name="DasyContract", include_abi=True) -> CompilerData:
